a921d7c2159d0afb654e760ebef031296cff5517
genti <gentius2@gmail.com>
2018-03-18 12:36:38 -0400

    Initial commit

---
+++ src/test/java/com/anaptec/gentihaddop/JUnitTestSuite.java
@@ -0,0 +1,17 @@
+package com.anaptec.gentihaddop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This is the test suite class to run all test classes with all their
+ * test cases
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AppTest.class,
+})
+
+public class JUnitTestSuite {
+}
